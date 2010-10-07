7fa1bbcabbf948a8a0006cfbd9932d12c6bccfb6
ianzhang <ian.zhang@sv.cmu.edu>
2010-10-07 15:26:39 -0700

    Linked pages together and several UI work on future storycards

--- app/controllers/deliverable_type_controller.rb
+++ app/controllers/deliverable_type_controller.rb
@@ -17,14 +17,13 @@ class DeliverableTypeController < ApplicationController
 
     # TODO: swap these for integration testing
     #@deliverable.project_id = session[:project_id]
-    @deliverable.project_id = 1000
-    @deliverable.phase = "temp"
+    @deliverable.project_id = session[:project_id]
+    @deliverable.phase = session[:phase]
 
     respond_to do |format|
       if @deliverable.save
         format.html{ redirect_to :controller => "deliverables" }
       else
-        puts "BAD"
         format.html{ render :action => "new", :status => :unprocessable_entity}
         #format.xml { render :xml => @deliverable.errors, :status => :unprocessable_entity }
       end
