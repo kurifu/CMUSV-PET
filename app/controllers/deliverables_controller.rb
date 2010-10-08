d784e008c5fb7e74bcd1d52165d1db2442618170
ianzhang <ian.zhang@sv.cmu.edu>
2010-10-08 14:15:51 -0700

    Fixed bugs on phase page

--- app/controllers/deliverables_controller.rb
+++ app/controllers/deliverables_controller.rb
@@ -2,22 +2,39 @@ class DeliverablesController < ApplicationController
 
   # Phase Page
   def index
+    session[:phase] = nil
     @deliverable = Deliverable.new
     project_id = session[:project_id]
     lifecycle = Project.find(project_id).lifecycle
     @phases = Project.get_phase(lifecycle)
     @deliverables_of_phase = []#Project.find(session[:project_id]).deliverables.find_all_by_phase("Requirements Gathering And Analysis")
-    @test = "in index"
     @deliverable_types =[]
   end
 
   def update_deliverable_partial
     @deliverables_of_phase = Project.find(session[:project_id]).deliverables.find_all_by_phase(params[:phase])
-    id = Project.identify_deliverable_type(params[:phase])
-    @deliverable_types = Project.get_deliverable_type(id)
-    session[:phase] = params[:phase]
-    render :update do |page|
-      page.replace_html 'phase_partial', :partial => 'deliverable_partial', :object => @deliverables_of_phase
+
+    if params[:phase].blank?
+      session[:phase] = params[:phase]
+      flash[:notice] = "Please select a phase"
+      redirect_to :action => "index"
+    else
+      id = Project.identify_deliverable_type(params[:phase])
+      @deliverable_types = Project.get_deliverable_type(id)
+      session[:phase] = params[:phase]
+      render :update do |page|
+        page.replace_html 'phase_partial', :partial => 'deliverable_partial', :object => @deliverables_of_phase
+      end
+    end
+    
+  end
+
+  def validate_before_adding_new_type
+    if session[:phase].blank?
+      flash[:notice] = "Please select a phase"
+      redirect_to :action => "index"
+    else
+      redirect_to :controller => "deliverable_type", :action => "new"
     end
   end
 
