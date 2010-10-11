cc2f0cf1304a29251a644ddee530bdc8c3773aff
ianzhang <ian.zhang@sv.cmu.edu>
2010-10-10 20:00:39 -0700

    added comments on project and deliverable model

--- app/models/deliverable.rb
+++ app/models/deliverable.rb
@@ -1,3 +1,24 @@
+# The deliverables table contains the following column
+#   id                integer
+#   project_id        integer
+#   name              varchar(255)
+#   deliverable_url   varchar(255)
+#   complexity        varchar(255)
+#   unit_measurement  varchar(255)
+#   estimated_size    varchar(255)
+#   estimated_effort  varchar(255)
+#   production_rate   varchar(255)
+#   deliverable_type  varchar(255)
+#   phase             varchar(255)
+#   description       text
+#
+# In the deliverable model class, it will validate the presence of deliverable_type,
+# phase, project_id, name, complexity, unit_measurement, estimated_size,
+# estimated_effort, production_rate.
+# It will also check the format of deliverable_url and the numericality of project_id
+#
+#
+
 class Deliverable < ActiveRecord::Base
 
   validates_presence_of :deliverable_type,:phase, :project_id, :name,
@@ -8,4 +29,6 @@ class Deliverable < ActiveRecord::Base
     :message  => 'must be in /deliverables folder',
     :unless => Proc.new { |u| u.deliverable_url.blank?}
   validates_numericality_of :project_id, :only_integer => true
+
+  belongs_to project
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,13 @@
+# The projects table contains the following column
+#   id            integer
+#   name          varchar(255)
+#   description   varchar(255)
+#   lifecycle     varchar(255)
+#   status        varchar(255)
+#
+# In the project model class, it will validate the presence of name and lifecycle.
+# It also that the name attribute should be unique.
+
 class Project < ActiveRecord::Base
   validates_uniqueness_of :name
   validates_presence_of :name, :lifecycle
