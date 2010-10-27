436450b39b9b81ec693d132a9fbc28588cd38c7f
Rahul Baxi <rahul.baxi@sv.cmu.edu>
2010-10-27 13:06:45 -0700

    Moved pet calculate code into javascript

--- app/controllers/deliverable_type_controller.rb
+++ app/controllers/deliverable_type_controller.rb
@@ -20,6 +20,11 @@ class DeliverableTypeController < ApplicationController
     @deliverable.project_id = session[:project_id]
     @deliverable.phase = session[:phase]
 
+    #Experimental code
+    @estimated_size = params[:deliverable][:estimated_size] || ''
+    @production_rate = params[:deliverable][:production_rate] || ''
+    @estimated_effort = params[:deliverable][:estimated_effort] || ''
+
     respond_to do |format|
       if @deliverable.save
         format.html{ redirect_to :controller => "deliverables" }
@@ -30,81 +35,7 @@ class DeliverableTypeController < ApplicationController
     end
   end
 
-  #This solution probably need to be refactored
-  #Capture data from the estimated_size, production_rate and estimated_effort
-  def capture_calculation_data
-    puts "Captured size: #{params[:estimated_size]}"
-    puts "Captured rate: #{params[:production_rate]}"
-    puts "Captured effort: #{params[:estimated_effort]}"
-
-    field = params[:field]
-    if field == '1'
-      session[:estimated_size]=params[:estimated_size]
-    elsif field == '2'
-      session[:production_rate]=params[:production_rate]
-    elsif field == '3'
-      session[:estimated_effort]=params[:estimated_effort]
-    end
-
-    puts "Current Value: #{session[:estimated_size]}"
-    puts "Current Value: #{session[:production_rate]}"
-    puts "Current Value: #{session[:estimated_effort]}"
-  end
-
-  # Calculate our input data given 2 values and 1 blank
-  # TODO:  Refactor this into a more optimal design
-  # NOTE:  Small bug with using flash[:notice]! Since we only re-render the partial,
-  # =>     The flash[:notice] only shows up in the bottom of the page.
-  # =>     However, if you refresh the page after it flashes, it appears at the
-  # =>     top as well.  Can we create and flash another variable instead of notice?
-  def process_calc_inputs
-    
-    puts "CHECK Size: #{session[:estimated_size]}"
-    puts "CHECK Rate: #{session[:production_rate]}"
-    puts "CHECK Effort: #{session[:estimated_effort]}"
-
-    rate = session[:production_rate]
-    size = session[:estimated_size]
-    effort = session[:estimated_effort]
-
-    # Error: All 3 blank or all 3 filled
-    if (effort.blank? && rate.blank? && size.blank?) ||
-        (!effort.blank? && !rate.blank? && !size.blank?)
-      flash[:notice_calc] = "Please enter two values"
-    else
-      # Error: Only 1 value
-      if effort.blank? && (rate.blank? || size.blank?)
-        flash[:notice_calc] = "Please enter two values"
-      elsif size.blank? && (effort.blank? || rate.blank?)
-        flash[:notice_calc] = "Please enter two values"
-      elsif rate.blank? && (effort.blank? || size.blank?)
-        flash[:notice_calc] = "Please enter two values"
-
-      # Correct Case: 2 values
-      elsif effort.blank? && !rate.blank? && !size.blank?
-        puts "* Only effort blank"
-        session[:estimated_effort] = rate.to_f * size.to_f
-        @notice_calc = ""
-      elsif size.blank? && !rate.blank? && !effort.blank?
-        puts "* size blank"
-        session[:estimated_size] = effort.to_f / rate.to_f
-        @notice_calc = ""
-      elsif rate.blank? && !effort.blank? && !size.blank?
-        puts "* rate blank"
-        session[:production_rate] = effort.to_f / size.to_f
-        @notice_calc = ""
-      else
-        puts "ERROR!"
-        # TODO:  Raise some kind of exception which we can catch
-      end
-
-      puts "Calc'd Size: #{session[:estimated_size]}"
-      puts "Calc'd Rate: #{session[:production_rate]}"
-      puts "Calc'd Effort: #{session[:estimated_effort]}"
-    end
-
-    render :partial=>'pet_calc'
-  end
+  
 
 #This method initializes the static content to be populated in the dropdown lists
   private
--- public/javascripts/pet_calc.js
+++ public/javascripts/pet_calc.js
@@ -1,57 +1,247 @@
+check_rate = document.getElementById("check_rate").src
+check_effort = document.getElementById("check_effort").src
+check_size = document.getElementById("check_size").src
 
-function validate_size(){
-    if("" == document.getElementById("deliverable_estimated_size").value){
-        document.getElementById("check_size").src = "/images/incorrect.png"
+correct_pic = "/images/correct.png"
+incorrect_pic = "/images/incorrect.png"
+default_pic = "/images/default.png"
+
+check = "ass"
+function check_all(size,effort,rate){
+    if("" == size.value){
+        check_size = default_pic
     }
-    
+    else if(!("" == size.value) && !("" == rate.value)){
+        check_effort = default_pic
+        effort.disabled = true
+        check_size = correct_pic
+    }
+
+    else if(!("" == size.value) && !("" == effort.value)){
+        check_rate = default_pic
+        rate.disabled = true
+        check_size.src = correct_pic
+    }
+    else if("" == size.value &&(!("" == rate.value))
+        && (!("" == effort.value))){
+        check_size.src = default_pic
+        estimated_size.disabled = true
+    }
+    else if("" == size.value && rate.disabled && !("" == effort.value)){
+        rate.disabled = false
+        check_size = incorrect_pic
+        check_rate = default_pic
+    }
+    else if("" == size.value && effort.disabled && !("" == rate.value)){
+        effort.disabled = false
+        check_size = incorrect_pic
+        check_effort = default_pic
+    }
     else{
-        document.getElementById("check_size").src = "/images/correct.png"
+        check_size = correct_pic
     }
 }
-function validate_rate(){
-    if("" == document.getElementById("deliverable_production_rate").value){
-        document.getElementById("check_rate").src = "/images/incorrect.png"
+
+function validate_size(){
+    if(isNaN(document.getElementById("deliverable_estimated_size").value)){
+        document.getElementById("error_msg").
+            innerHTML = "Please enter numeric values"
+        document.getElementById("check_size").src = "/images/incorrect.png"
+    }
+    else{
+    if("" == document.getElementById("deliverable_estimated_size").value){
+        document.getElementById("check_size").src = "/images/incorrect.png"
     }
     else if(!("" == document.getElementById("deliverable_estimated_size").value)
         && !("" == document.getElementById("deliverable_production_rate").value)
         ){
-        document.getElementById("check_effort").src = ""
+        document.getElementById("check_effort").src = "/images/default.png"
+        document.getElementById("deliverable_estimated_effort").disabled="true"
+        document.getElementById("check_size").src = "/images/correct.png"
+    }
+    else if(!("" == document.getElementById("deliverable_estimated_size").value)
+        && !("" == document.getElementById("deliverable_estimated_effort").value)
+        ){
+        document.getElementById("check_rate").src = "/images/default.png"
+        document.getElementById("deliverable_production_rate").disabled="true"
+        document.getElementById("check_size").src = "/images/correct.png"
+    }
+    else if("" == document.getElementById("deliverable_estimated_size").value
+        &&(!("" == document.getElementById("deliverable_production_rate").value)
+        && (!("" == document.
+        getElementById("deliverable_estimated_effort").value)))){
+        document.getElementById("check_size").src = "/images/default.png"
+        document.getElementById("deliverable_estimated_size").disabled="true"
+    }
+
+//    else if("" == document.getElementById("deliverable_estimated_size").value
+//        && (document.getElementById("deliverable_production_rate").disabled) &&
+//        !("" == document.getElementById("deliverable_estimated_effort").value)){
+//        document.getElementById("deliverable_production_rate").disabled = false
+//        document.getElementById("check_size").src = "/images/incorrect.png"
+//        document.getElementById("check_rate").src = "/images/default.png"
+//    }
+//    else if("" == document.getElementById("deliverable_estimated_size").value
+//        && (document.getElementById("deliverable_estimated_effort").disabled) &&
+//        !("" == document.getElementById("deliverable_production_rate").value)){
+//        document.getElementById("deliverable_estimated_effort").disabled = false
+//        document.getElementById("check_size").src = "/images/incorrect.png"
+//        document.getElementById("check_effort").src = "/images/default.png"
+//    }
+    else{
+        document.getElementById("check_size").src = "/images/correct.png"
+    }
+    }
+}
+function validate_rate(){
+
+    if(isNaN(document.getElementById("deliverable_production_rate").value)){
+        document.getElementById("error_msg").
+            innerHTML = "Please enter numeric values"
+        document.getElementById("check_rate").src = "/images/incorrect.png"
+    }
+    else{
+
+    if("" == document.getElementById("deliverable_production_rate").value){
+        document.getElementById("check_rate").src = "/images/incorrect.png"
+    }
+    else if(!("" == document.getElementById("deliverable_estimated_effort").value)
+        && !("" == document.getElementById("deliverable_production_rate").value)
+        ){
+        document.getElementById("check_size").src = "/images/default.png"
+        document.getElementById("deliverable_estimated_size").disabled="true"
+        document.getElementById("check_rate").src = "/images/correct.png"
+    }
+    else if(!("" == document.getElementById("deliverable_estimated_size").value)
+        && !("" == document.getElementById("deliverable_production_rate").value)
+        ){
+        document.getElementById("check_effort").src = "/images/default.png"
         document.getElementById("deliverable_estimated_effort").disabled="true"
         document.getElementById("check_rate").src = "/images/correct.png"
+    }
+    else if("" == document.getElementById("deliverable_production_rate").value
+        &&(!("" == document.getElementById("deliverable_estimated_size").value)
+        && (!("" == document.getElementById("deliverable_estimated_effort").value)))){
+        document.getElementById("check_rate").src = "/images/default.png"
+        document.getElementById("deliverable_production_rate").disabled="true"
+    }
+    else if(document.getElementById("deliverable_estimated_size").disabled){
+            if(""==document.getElementById("deliverable_production_rate").value)
+                {
+                    document.getElementById("deliverable_estimated_size")
+                    .disabled="false"
+                     document.getElementById("check_rate")
+                     .src = "/images/incorrect.png"
+                    document.getElementById("check_size")
+                    .src = "/images/incorrect.png"
+                }
+
     }
     else{
         document.getElementById("check_rate").src = "/images/correct.png"
     }
+    }
 }
 function validate_effort(){
+
+    if(isNaN(document.getElementById("deliverable_estimated_effort").value)){
+        document.getElementById("error_msg").
+            innerHTML = "Please enter numeric values"
+        document.getElementById("check_effort").src = "/images/incorrect.png"
+    }
+    else{
     if("" == document.getElementById("deliverable_estimated_effort").value){
         document.getElementById("check_effort").src = "/images/incorrect.png"
     }
     else if(!("" == document.getElementById("deliverable_estimated_size").value)
         && !("" == document.getElementById("deliverable_estimated_effort").value)
         && ("" == document.getElementById("deliverable_production_rate").value)){
-        document.getElementById("check_rate").src = ""
+        document.getElementById("check_rate").src = "/images/default.png"
         document.getElementById("deliverable_production_rate").disabled="true"
         document.getElementById("check_effort").src = "/images/correct.png"
     }
     else if(!("" == document.getElementById("deliverable_production_rate").value)
         && !("" == document.getElementById("deliverable_estimated_effort").value)
         && ("" == document.getElementById("deliverable_estimated_size").value)){
-        document.getElementById("check_size").src = ""
+        document.getElementById("check_size").src = "/images/default.png"
         document.getElementById("deliverable_estimated_size").disabled="true"
         document.getElementById("check_effort").src = "/images/correct.png"
     }
     else{
         document.getElementById("check_effort").src = "/images/correct.png"
     }
+    }
 }
 
-function onchange_effort(){
+
+function reset(){
+
+    document.getElementById("deliverable_estimated_size").value=""
+    document.getElementById("deliverable_estimated_effort").value=""
+    document.getElementById("deliverable_production_rate").value=""
+
+    
+    document.getElementById("deliverable_estimated_size").disabled=false
+    document.getElementById("deliverable_estimated_effort").disabled=false
+    document.getElementById("deliverable_production_rate").disabled=false
+    
+    document.getElementById("check_rate").src="/images/default.png"
+    document.getElementById("check_size").src="/images/default.png"
+    document.getElementById("check_effort").src="/images/default.png"
+
+    document.getElementById("error_msg").innerHTML = ""
+
+}
+
+function calculate(){
+
+    
     if("" == document.getElementById("deliverable_estimated_size").value &&
-        !("" == document.getElementById("deliverable_production_rate").value)&&
-    !("" == document.getElementById("deliverable_estimated_effort").value)){
-        document.getElementById("delveriable_estimated_size").disabled="true"
-        document.getElementById("check_size").src = ""
-        
+       "" == document.getElementById("deliverable_estimated_effort").value &&
+       "" == document.getElementById("deliverable_production_rate").value ){
+        document.getElementById("error_msg")
+        .innerHTML = "Please enter two values";
     }
+  
+    else{
+         document.getElementById("error_msg")
+        .innerHTML = "";
+    if(!("" == document.getElementById("deliverable_estimated_size").value) &&
+       !("" == document.getElementById("deliverable_estimated_effort").value)){
+            size = parseFloat(document.getElementById
+            ("deliverable_estimated_size").value)
+            effort = parseFloat(document.getElementById
+            ("deliverable_estimated_effort").value)
+         rate = effort/size
+        document.getElementById("deliverable_production_rate").value = rate
+        document.getElementById("deliverable_production_rate").disabled=false
+        document.getElementById("check_size").src = "/images/default.png"
+        document.getElementById("check_effort").src = "/images/default.png"
+        }
+   else if(!("" == document.getElementById("deliverable_estimated_size").value)
+      && !("" == document.getElementById("deliverable_production_rate").value)){
+            size = parseFloat(document.getElementById
+            ("deliverable_estimated_size").value)
+            rate = parseFloat(document.getElementById
+            ("deliverable_production_rate").value)
+         effort = size * rate
+        document.getElementById("deliverable_estimated_effort").value = effort
+        document.getElementById("deliverable_estimated_effort").disabled=false
+        document.getElementById("check_size").src = "/images/default.png"
+        document.getElementById("check_rate").src = "/images/default.png"
+        }
+    else if(!("" == document.getElementById("deliverable_estimated_effort")
+    .value) &&
+       !("" == document.getElementById("deliverable_production_rate").value)){
+            rate = parseFloat(document.getElementById
+            ("deliverable_production_rate").value)
+            effort = parseFloat(document.getElementById
+            ("deliverable_estimated_effort").value)
+         size = effort / rate
+        document.getElementById("deliverable_estimated_size").value = size
+        document.getElementById("deliverable_estimated_size").disabled=false
+        document.getElementById("check_effort").src = "/images/default.png"
+        document.getElementById("check_rate").src = "/images/default.png"
+        }
+}
 }
