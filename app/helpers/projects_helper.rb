9f3bdda1f3ab52e2d721b83815d757e13c42bd05
Cliff Chen <cliff.chen@sv.cmu.edu>
2010-11-04 19:01:25 -0700

    fixed styles + calculate_phase_effort, somehow it was deleted?

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,20 @@
 module ProjectsHelper
+  #(@project.id, phase)
+  def calculate_phase_effort(project_id, phase)
+    puts "Input id #{project_id}, Input phase #{phase}"
+
+    @hours = 0
+    @dataset = Deliverable.find_all_by_phase_and_project_id(phase, project_id)
+    puts @dataset
+    if @dataset.nil?
+      return 0
+    else
+      @dataset.each do |d|
+        @hours += d.estimated_effort.to_f
+      end
+      return @hours
+    end
+
+
+  end
 end
