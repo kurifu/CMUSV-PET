3fc08be3829c2e1b68f2f48408e9fa41af3fea56
ianzhang <ian.zhang@sv.cmu.edu>
2010-11-16 18:14:15 -0800

    fixed rspec test

--- spec/controllers/deliverable_type_controller_spec.rb
+++ spec/controllers/deliverable_type_controller_spec.rb
@@ -34,39 +34,6 @@ describe DeliverableTypeController do
       response.should have_tag('table#historical_data tr#effort')
       response.should have_tag('table#historical_data tr#effort td', :count => 4)
       #response.should have_tag('a[href=?]', "#", :text => "Reset")
-  
-  it "should redirect to login page if not logged in" do
-    @user_session.destroy
-    get :new
-    response.should redirect_to login_path
-  end
-
-  it "should show the new deliverable page" do
-    # Random phase name, doesn't matter what
-    session[:phase] = "System Design"
-    get 'new'
-    response.should render_template("deliverable_type/new")
-    response.should have_tag('div.content')
-    response.should have_tag('table#pet_table')
-    response.should have_tag('div#pet_calc_div')
-    #response.should have_tag('a[href=?]', "#", :text => "Calculate")
-    #response.should have_tag('a[href=?]', "#", :text => "Reset")
-
-    # Historical Data link
-    response.should have_tag('a[href=?]', "#", :text => "Historical Data")
-
-    response.should have_tag "form[action=/deliverable_type/create]" do
-      with_tag "select[name='deliverable[deliverable_type]']"
-      with_tag "input[type=text][name='deliverable[name]']"
-      with_tag "textarea[name='deliverable[description]']"
-      with_tag "select[name='deliverable[complexity]']"
-      with_tag "select[name='deliverable[unit_measurement]']"
-      with_tag "input[type=text][name='deliverable[estimated_size]']"
-      with_tag "input[type=text][name='deliverable[production_rate]']"
-      with_tag "input[type=text][name='deliverable[estimated_effort]']"
-      with_tag "input[type=submit][name='commit'][id=deliverable_submit]"
-    end
-  end
 
 
       # Historical Data link
@@ -84,6 +51,39 @@ describe DeliverableTypeController do
         with_tag "input[type=submit][name='commit'][id=deliverable_submit]"
       end
     end
+
+    it "should redirect to login page if not logged in" do
+    @user_session.destroy
+    get :new
+    response.should redirect_to login_path
+  end
+
+  it "should show the new deliverable page" do
+    # Random phase name, doesn't matter what
+    session[:phase] = "System Design"
+    get 'new'
+    response.should render_template("deliverable_type/new")
+    response.should have_tag('div.content')
+    response.should have_tag('table#pet_table')
+    response.should have_tag('div#pet_calc_div')
+    #response.should have_tag('a[href=?]', "#", :text => "Calculate")
+    #response.should have_tag('a[href=?]', "#", :text => "Reset")
+
+    # Historical Data link
+#    response.should have_tag('a[href=?]', "#", :text => "Historical Data")
+
+    response.should have_tag "form[action=/deliverable_type/create]" do
+      with_tag "select[name='deliverable[deliverable_type]']"
+      with_tag "input[type=text][name='deliverable[name]']"
+      with_tag "textarea[name='deliverable[description]']"
+      with_tag "select[name='deliverable[complexity]']"
+      with_tag "select[name='deliverable[unit_measurement]']"
+      with_tag "input[type=text][name='deliverable[estimated_size]']"
+      with_tag "input[type=text][name='deliverable[production_rate]']"
+      with_tag "input[type=text][name='deliverable[estimated_effort]']"
+      with_tag "input[type=submit][name='commit'][id=deliverable_submit]"
+    end
+  end
   end
 
   describe "CREATE a Deliverable Type" do
