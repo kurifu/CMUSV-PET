affce0a49e66de973644a4a5d513bb3e601377e0
Rahul Baxi <rahul.baxi@sv.cmu.edu>
2010-11-23 00:39:22 -0800

    Made Cosmetic changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-#before_filter :require_user
-#before_filter :require_admin, :only => [:new, :edit, :udpate, :destroy, :index, :create]
+before_filter :require_user
+before_filter :require_admin, :only => [:new, :edit, :udpate, :destroy, :index, :create]
 
   # GET /users
   # GET /users.xml
