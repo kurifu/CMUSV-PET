1539ef807d12ea5d12470cb245075e48dc7d276f
ianzhang <ian.zhang@sv.cmu.edu>
2010-11-23 00:05:35 -0800

    added two test cases for storycard17

--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -23,14 +23,25 @@ describe UserSessionsController do
   describe "Valid User" do
 
     it "should redirect user to User home screen" do
-     
+      UserSession.stubs(:find).returns(UserSession.new)
+      UserSession.any_instance.stubs(:user).returns(User.new)
       UserSession.any_instance.stubs(:save).returns(true)
-      User.any_instance.stubs(:user_class).returns("admin")
-      ApplicationController.stubs(:current_user_admin).returns(false)
+      User.any_instance.stubs(:user_class).returns("Regular")
+      #ApplicationController.stubs(:current_user_admin).returns(false)
       post :create
       response.should redirect_to(root_path)
-
     end
+
+    it "should redirect admin to admin home screen" do
+      UserSession.stubs(:find).returns(UserSession.new)
+      UserSession.any_instance.stubs(:user).returns(User.new)
+      UserSession.any_instance.stubs(:save).returns(true)
+      User.any_instance.stubs(:user_class).returns("admin")
+      #ApplicationController.stubs(:current_user_admin).returns(false)
+      post :create
+      response.should redirect_to(admin_home_path)
+    end
+
     it "logout valid user and show proper message" do
       attr = {:username=>'invalid', :password=>'invalid'}
       user_session = UserSession.new(attr)
