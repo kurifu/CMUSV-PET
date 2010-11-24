9b4e19a53673e2af2d8d13ef0718bbaab87e5793
Rahul Baxi <rahul.baxi@sv.cmu.edu>
2010-11-24 01:58:18 -0800

    Removed dirty code from pet_calc.js and _pet_calc.html.erb

--- public/javascripts/pet_calc.js
+++ public/javascripts/pet_calc.js
@@ -6,37 +6,7 @@ correct_pic = "/images/correct.png"
 incorrect_pic = "/images/incorrect.png"
 default_pic = "/images/default.png"
 
-check = "ass"
-function check_all(size,effort,rate){
-    if("" == size.value){
-        check_size = default_pic
-    }
-    else if(!("" == size.value) && !("" == rate.value)){
-        check_effort = default_pic
-        check_size = correct_pic
-    }
 
-    else if(!("" == size.value) && !("" == effort.value)){
-        check_rate = default_pic
-       check_size.src = correct_pic
-    }
-    else if("" == size.value &&(!("" == rate.value))
-        && (!("" == effort.value))){
-        check_size.src = default_pic
-       
-    }
-    else if("" == size.value && rate.disabled && !("" == effort.value)){
-       check_size = incorrect_pic
-        check_rate = default_pic
-    }
-    else if("" == size.value && effort.disabled && !("" == rate.value)){
-        check_size = incorrect_pic
-        check_effort = default_pic
-    }
-    else{
-        check_size = correct_pic
-    }
-}
 
 function validate_size(){
     if(isNaN(document.getElementById("deliverable_estimated_size").value)){
@@ -52,14 +22,12 @@ function validate_size(){
         && !("" == document.getElementById("deliverable_production_rate").value)
         ){
         document.getElementById("check_effort").src = "/images/default.png"
-        document.getElementById("deliverable_estimated_effort").disabled="true"
-        document.getElementById("check_size").src = "/images/correct.png"
+       document.getElementById("check_size").src = "/images/correct.png"
     }
     else if(!("" == document.getElementById("deliverable_estimated_size").value)
         && !("" == document.getElementById("deliverable_estimated_effort").value)
         ){
         document.getElementById("check_rate").src = "/images/default.png"
-        document.getElementById("deliverable_production_rate").disabled="true"
         document.getElementById("check_size").src = "/images/correct.png"
     }
     else if("" == document.getElementById("deliverable_estimated_size").value
@@ -67,7 +35,7 @@ function validate_size(){
         && (!("" == document.
         getElementById("deliverable_estimated_effort").value)))){
         document.getElementById("check_size").src = "/images/default.png"
-        document.getElementById("deliverable_estimated_size").disabled="true"
+       
     }
 
     else{
@@ -87,7 +55,7 @@ function validate_rate(){
     if("" == document.getElementById("deliverable_production_rate").value){
         document.getElementById("check_rate").src = "/images/incorrect.png"
     }
-    else if(!("" == document.getElementById("deliverable_estimated_effort").value)
+     else if(!("" == document.getElementById("deliverable_estimated_effort").value)
         && !("" == document.getElementById("deliverable_production_rate").value)
         ){
         document.getElementById("check_size").src = "/images/default.png"
@@ -108,6 +76,8 @@ function validate_rate(){
     else if(document.getElementById("deliverable_estimated_size").disabled){
             if(""==document.getElementById("deliverable_production_rate").value)
                 {
+                    document.getElementById("deliverable_estimated_size")
+                    .disabled="false"
                      document.getElementById("check_rate")
                      .src = "/images/incorrect.png"
                     document.getElementById("check_size")
@@ -156,7 +126,11 @@ function reset(){
     document.getElementById("deliverable_estimated_effort").value=""
     document.getElementById("deliverable_production_rate").value=""
 
-    
+
+    document.getElementById("deliverable_estimated_size").disabled=false
+    document.getElementById("deliverable_estimated_effort").disabled=false
+    document.getElementById("deliverable_production_rate").disabled=false
+
     document.getElementById("check_rate").src="/images/default.png"
     document.getElementById("check_size").src="/images/default.png"
     document.getElementById("check_effort").src="/images/default.png"
@@ -174,7 +148,16 @@ function calculate(){
         document.getElementById("calc")
         .innerHTML = "Please enter two values";
     }
-  
+
+    else if(!("" == document.getElementById("deliverable_estimated_size")
+            .value) &&
+            !("" == document.getElementById("deliverable_estimated_effort")
+            .value) &&
+                !("" == document.getElementById("deliverable_production_rate")
+            .value)){
+                document.getElementById("calc")
+        .innerHTML = "Please remove one of the three values for correct calculation";
+            }
     else{
          document.getElementById("calc")
         .innerHTML = "";
@@ -188,6 +171,7 @@ function calculate(){
         document.getElementById("deliverable_production_rate").value = rate
         document.getElementById("check_size").src = "/images/default.png"
         document.getElementById("check_effort").src = "/images/default.png"
+        document.getElementById("check_rate").src = "/images/default.png"
         }
    else if(!("" == document.getElementById("deliverable_estimated_size").value)
       && !("" == document.getElementById("deliverable_production_rate").value)){
@@ -199,6 +183,7 @@ function calculate(){
         document.getElementById("deliverable_estimated_effort").value = effort
         document.getElementById("check_size").src = "/images/default.png"
         document.getElementById("check_rate").src = "/images/default.png"
+        document.getElementById("check_effort").src = "/images/default.png"
         }
     else if(!("" == document.getElementById("deliverable_estimated_effort")
     .value) &&
@@ -209,8 +194,11 @@ function calculate(){
             ("deliverable_estimated_effort").value)
          size = effort / rate
         document.getElementById("deliverable_estimated_size").value = size
-        document.getElementById("deliverable_estimated_size").disabled=false
+        document.getElementById("check_effort").src = "/images/default.png"
         document.getElementById("check_rate").src = "/images/default.png"
+        document.getElementById("check_size").src = "/images/default.png"
         }
+       
 }
+
 }
